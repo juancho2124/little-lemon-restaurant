bf193821ce798a8941e12b0501d6ab3c12cff58f
juancho2124 <juandavidpo93@gmail.com>
2024-01-24 15:28:28 -0500

    hero and specials components implementation

--- little-lemon-retaurant/src/components/HeroSection.js
+++ little-lemon-retaurant/src/components/HeroSection.js
@@ -1,7 +1,37 @@
 import React from "react";
+import HeroImage from "../Assets/heroimg.jpg";
+import "../styles/HeroSection.css";
+import { NavLink } from "react-router-dom";
 
 const HeroSection = () => {
-  return <div>HeroSection</div>;
+  return (
+    <div className="hero-container">
+      <div className="hero-section-background">
+        <div className="hero-content">
+          <div className="hero-text">
+            <h1>Little Lemon</h1>
+            <h2>Chicago</h2>
+            <p>
+              Welcome to Little Lemon - Your Mediterranean Escape! In our
+              family-owned haven, we blend cherished traditional recipes with
+              innovative, modern twists. Dive into a culinary journey that spans
+              from the rustic coasts of Greece to the vibrant markets of
+              Morocco, all in the comfort of our cozy, welcoming eatery.
+              Experience the fusion of generations-old Mediterranean flavors and
+              contemporary culinary artistry. Join us for a taste of tradition
+              reimagined.
+            </p>
+            <NavLink to="/reservations">
+              <button className="btn">Reserve a Table</button>
+            </NavLink>
+          </div>
+          <div className="img-container">
+            <img className="hero-img" src={HeroImage} alt="Little Lemon Dish" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HeroSection;
--- little-lemon-retaurant/src/components/Specials.js
+++ little-lemon-retaurant/src/components/Specials.js
@@ -1,7 +1,63 @@
 import React from "react";
 
+import "../styles/Specials.css";
+import greeksalad from "../Assets/greeksalad.jpg";
+import lemondessert from "../Assets/lemondessert.jpg";
+import bruchetta from "../Assets/bruchetta.jpg";
+
 const Specials = () => {
-  return <div>Specials</div>;
+  const specialMenus = [
+    {
+      image: greeksalad,
+      title: "Greek Salad",
+      price: "$12.99",
+      description:
+        "The famous Greek salad of crispy lettuce, peppers, olives and our Chicago styled feta cheese, garnished with crunchy garlic, rosemary croutons.",
+      order: "Order a delivery",
+    },
+    {
+      image: bruchetta,
+      title: "Bruschetta",
+      price: "$5.99",
+      description:
+        "Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil; made perfect for an evening dinner.",
+      order: "Order a delivery",
+    },
+    {
+      image: lemondessert,
+      title: "Lemon Dessert",
+      price: "$5.00",
+      description:
+        "This comes straight from Grandma's recipe book. Every last ingredient has been sourced and is as authentic as can be imagined.",
+      order: "Order a delivery",
+    },
+  ];
+
+  return (
+    <div className="specials-container">
+      <div className="menu-container">
+        <div className="menu">
+          <div>
+            <h1>This weeks specials!</h1>
+            <button className="btn">Online Menu</button>
+          </div>
+          <div className="card-container">
+            {specialMenus.map((menu, index) => (
+              <div key={index} className="menu-card">
+                <img src={menu.image} alt={menu.title} />
+                <div className="heading-container">
+                  <h2>{menu.title}</h2>
+                  <h3>{menu.price}</h3>
+                </div>
+                <p>{menu.description}</p>
+                <button className="order-button">{menu.order}</button>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Specials;
