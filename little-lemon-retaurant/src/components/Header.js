8cb3739c0d90b0c5873669026fca6a5ff1ee3898
juancho2124 <juandavidpo93@gmail.com>
2024-01-24 13:31:45 -0500

    header component implementation

--- little-lemon-retaurant/src/components/Header.js
+++ little-lemon-retaurant/src/components/Header.js
@@ -1,7 +1,38 @@
 import React from "react";
+import logo from "../Assets/logo.jpg";
+import "../styles/Header.css";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <div className="image-wrapper">
+        <NavLink to="/">{<img src={logo} alt="Little Lemon Logo" />}</NavLink>
+      </div>
+      <nav className="navbar">
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="#">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="#">Menu</NavLink>
+          </li>
+          <li>
+            <NavLink to="/reservations">Reservations</NavLink>
+          </li>
+          <li>
+            <NavLink to="#">Order Online</NavLink>
+          </li>
+          <li>
+            <NavLink to="#">Login</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
