cd5b46e698beb79db439a25831eb14554e2d179d
juancho2124 <juandavidpo93@gmail.com>
2024-01-27 18:25:59 -0500

    media queries and refactoring

--- little-lemon-retaurant/src/components/Header.js
+++ little-lemon-retaurant/src/components/Header.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../Assets/logo.jpg";
 import "../styles/Header.css";
 import { NavLink } from "react-router-dom";
+import { HiBars4, HiOutlineXMark } from "react-icons/hi2";
 
 const Header = () => {
+  const [isNavVisible, setIsNavVisible] = useState(false);
+  const toggleNav = () => {
+    setIsNavVisible(!isNavVisible);
+  };
   return (
     <header className="header">
       <div className="image-wrapper">
         <NavLink to="/">{<img src={logo} alt="Little Lemon Logo" />}</NavLink>
       </div>
-      <nav className="navbar">
+      <button className="hamburger-menu" onClick={toggleNav}>
+        {isNavVisible ? <HiOutlineXMark /> : <HiBars4 />}
+      </button>
+      <nav className={`navbar ${isNavVisible ? "show" : ""}`}>
         <ul>
           <li>
             <NavLink to="/">Home</NavLink>
